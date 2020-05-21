fb7a78381dab8c3ccea019b54922f4467048b789
Jan Schreiber <jans@ravn.no>
2020-05-21 15:28:00 +0200

    Script for generating .gmi files from markdown

---
+++ generate-gemini.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# Converts md-files to gemini files
+# Gemini is a new internet protocol, see https://gemini.circumlunar.space/
+# for details.
+# md2gemini is available trough "pip install md2gemini"
+DIRS=`find . -type d -mindepth 1 | grep -v git`
+
+for dir in $DIRS
+do
+  FILES=`find $dir -name \*.md`
+  echo $FILES
+  for filename in $FILES
+  do
+    file="${filename%.*}"
+    md2gemini $file.md > $file.gmi
+    #pandoc $file.md > $file.gmi
+  done
+done
+
+md2gemini README.md > index.gmi
+sed -i'.bak' -e 's/\.md/\.gmi/g' index.gmi
+rm index.gmi.bak
+
+FILES=`find . -name \*.gmi`
+for file in $FILES
+do
+  DIR=$(dirname "${file}")
+  mkdir -p ~/gemini/$DIR
+  mv $file ~/gemini/$file
+done
+
